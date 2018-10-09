32b1471e35ca64667bf266931c6061f681d8fa5e
dagdafrench <39657662+dagdafrench@users.noreply.github.com>
2018-10-09 04:11:59 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -8,8 +8,7 @@ function sleep(seconds){
 describe('Random test.', function() {
 
   it('should return -1 when the value is not present', function() {
-    this.timeout(100000000000);
-    sleep(120);
+    sleep(30);
     assert.equal([1,2,3].indexOf(4), -1);
   });
 
